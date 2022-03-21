cb0ac1d3ed476d9c7bdeca5754338f0c21fdb7f1
Shannon Smith <silveraldrin@gmail.com>
2022-03-21 23:51:52 +0530

    Changed UI

--- resume/urls.py
+++ resume/urls.py
@@ -7,6 +7,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- resume/views.py
+++ resume/views.py
@@ -1,16 +1,14 @@
 from django.http import HttpResponse
+from django.http import JsonResponse
 import json
-from .models import User
+from .models import User,Education, Certification
 from django.template import loader
 
+template = loader.get_template('resumes/index.html')
 
 def index(request):
 
-    my_list = []
-
-
     user_details = User.objects.all()
-    template = loader.get_template('resumes/index.html')
 
     for details in user_details:
         user_dict = {}
@@ -23,14 +21,47 @@ def index(request):
         hobby_list.append(details.hobby2)
         hobby_list.append(details.hobby3)
         user_dict["hobbies"] = hobby_list
-        #my_list.append(user_dict)
 
-    uploaded = json.dumps(user_dict)
+    user_uploaded = json.dumps(user_dict)
+
+
+    education_details = Education.objects.all()
+
+    for education_item in education_details:
+        education = {}
+        education["degree"] = education_item.degree
+        education["higher national diploma [Edexcel UK]"] = education_item.degree
+
+    education_uploaded = json.dumps(education)
+
+    certification_items = Certification.objects.all()
+
+    for certification_item in certification_items:
+        certification = {}
+        multi_aws = []
+        certification["ITIL"] = certification_item.certification1
+        certification["Kubernetes"] = certification_item.certification2
+        multi_aws.append(certification_item.certification3)
+        multi_aws.append(certification_item.certification4)
+        multi_aws.append(certification_item.certification5)
+        certification["AWS"] = multi_aws
+
+
+
 
     context = {
-        'User_details' : uploaded
+        'User_details' : user_uploaded,
+        'education_details': education_uploaded,
+        'certification_details': certification
     }
-    print(context)
 
-    #return HttpResponse(uploaded)
+
+    print(type(str(context)))
+
     return HttpResponse(template.render(context,request))
+    #return JsonResponse(user_dict, json_dumps_params={'indent': 2})
+
+
+
+
+
